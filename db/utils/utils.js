8ca8b912f5a6466fd258b254ccfb65bb4ec0e2a3
NaomiM24 <naomimercer@hotmail.co.uk>
2019-10-28 14:37:56 +0000

    format dates and makeRefObj utils complete

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,5 +1,30 @@
-exports.formatDates = list => {};
+exports.formatDates = list => {
+  const convertedDate = list.map(item => new Date(item))
+  console.log(convertedDate)
+  return convertedDate
+};
 
-exports.makeRefObj = list => {};
+exports.makeRefObj = list => {
+  const refObj = {};
+  list.forEach(item => {
+    refObj[item.title] = item.article_id;
+  });
+  return refObj;
+};
 
-exports.formatComments = (comments, articleRef) => {};
+exports.formatComments = (comments, articleRef) => {
+  const refObj = {};
+  
+};
+
+/*## formatComments
+
+This utility function should be able to take an array of comment objects (`comments`) and a reference object, and return a new array of formatted comments.
+
+Each formatted comment must have:
+
+- Its `created_by` property renamed to an `author` key
+- Its `belongs_to` property renamed to an `article_id` key
+- The value of the new `article_id` key must be the id corresponding to the original title value provided
+- Its `created_at` value converted into a javascript date object
+- The rest of the comment's properties must be maintained */
--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -5,8 +5,32 @@ const {
   formatComments,
 } = require('../db/utils/utils');
 
-describe('formatDates', () => {});
+describe('formatDates', () => {
+  it('returns a new empty array when passed an empty array', () => {
+    expect(formatDates([])).to.eql([])
+  })
+  it('returns an array with one converted data when passed an array with one date obect', () => {
+    expect(formatDates([1542284514171])).to.eql([new Date(1542284514171)])
+  })
+  it('returns a new array', () => {
+    const input = [1542284514171]
+    expect(formatDates(input)).to.not.eql(input)
+  })
+  it('returns a new array with more than one converted data when passed an array with more than one date obect', () => {
+    expect(formatDates([1542284514171, 1416140514171])).to.eql([new Date(1542284514171), new Date(1416140514171)])
+  })
+});
 
-describe('makeRefObj', () => {});
+describe('makeRefObj', () => {
+  it('rteurns an empty object when passed an empty array', ()=> {
+    expect(makeRefObj([])).to.eql({});
+  })
+  it('returns one object with a reference key when passed an array with one object', () => {
+    expect(makeRefObj([{ article_id: 1, title: 'A' }])).to.eql({ A: 1 })
+  })
+  it('returns an object with multiple reference keys', () =>{
+    expect(makeRefObj([{ article_id: 1, title: 'A' }, {article_id: 2, title: 'B'}])).to.eql({A: 1, B: 2})
+  })
+});
 
 describe('formatComments', () => {});
