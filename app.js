80285fe6f179482bcba5f1b21eeab71a818d8480
NaomiM24 <naomimercer@hotmail.co.uk>
2019-11-18 13:16:42 +0000

    added cors

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const apiRouter = require("./routers/api-router");
-const {psqlErrors, customErrors, serverErrors} = require('./errors/error-handling')
+const {psqlErrors, customErrors, serverErrors} = require('./errors/error-handling');
+const cors = require('cors');
+
+app.use(cors());
 
 app.use(express.json());
 
